3f5ca7e5cabb4afa796f69872125d19f2efab2d8
diirector <jozef.kubikml@gmail.com>
2020-02-24 21:42:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This project is under heavy development.
+Some information will be added very soon...
+
+_...maybe_
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    //nieco
+?>
+
+<!DOCTYPE html>
+
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Jozef Kubík ml.">
+
+    <title>Absolutely not a Bachelor thesis</title>
+</head>
+
+<body>
+    <p>Move on, do not stop here... Nothing to see...</p>
+</body>
+</html>
